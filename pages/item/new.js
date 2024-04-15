b0ea0c1d4c9dcdc7433364717f3f52e5464ba75f
ryanmbigelow <ryanmbigelow@gmail.com>
2024-04-15 16:00:55 -0500

    created item/new page

---
+++ pages/item/new.js
@@ -0,0 +1,10 @@
+// import ItemForm from '../../components/Forms/ItemForm';
+
+const NewItem = () => (
+  <div>
+    <h2>Add New Item</h2>
+    {/* <ItemForm /> */}
+  </div>
+);
+
+export default NewItem;
