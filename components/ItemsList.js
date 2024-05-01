a6f4339461983fe13f1e84e91bded56c3970ec8f
ryanmbigelow <ryanmbigelow@gmail.com>
2024-05-01 18:07:37 -0500

    still working on items list

--- components/ItemsList.js
+++ components/ItemsList.js
@@ -1,33 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 import styles from '../styles/Item.module.css';
-import { getItems } from '../api/itemsData';
-// eslint-disable-next-line react/prop-types
+
 export default function ItemsList({ itemsObj }) {
-  const [itemz, setItemz] = useState([]);
-  useEffect(() => {
-    getItems().then(setItemz);
-  }, []);
   return (
     <div className={styles.container}>
       <div className={styles.grid}>
-        {' '}
-        {itemz?.map((item) => (
-          // eslint-disable-next-line @next/next/link-passhref
-          <>
-            <Link passHref href="/items/[id]" as={`/items/${item.id}`}>
-              <div className={styles.card}>
-                <h3>{itemsObj.name} &rarr;</h3>
-                <p>{itemsObj.description}</p>
-                <p>{itemsObj.image}</p>
-                <p>Price: ${itemsObj.price}</p>
-                {/* <p>Category:{itemsObj.category_id.name}</p> */}
-                <p>Size: {itemsObj.size}</p>
-              </div>
-            </Link>
-          </>
-        ))}
+        <>
+          <Link passHref href="/items/[id]" as={`/items/${itemsObj.id}`}>
+            <div className={styles.card}>
+              <h3>{itemsObj.name} &rarr;</h3>
+              <p>{itemsObj.description}</p>
+              <p>{itemsObj.image}</p>
+              <p>Price: ${itemsObj.price}</p>
+              <p>Category:{itemsObj.categoryId.name}</p>
+              <p>Size: {itemsObj.size}</p>
+            </div>
+          </Link>
+        </>
       </div>
     </div>
   );
@@ -39,10 +30,10 @@ ItemsList.propTypes = {
     name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
-    price: PropTypes.string.isRequired,
-    category_id: PropTypes.shape({
+    price: PropTypes.number.isRequired,
+    categoryId: PropTypes.shape({
       id: PropTypes.number.isRequired,
-      name: PropTypes.bool.isRequired,
+      name: PropTypes.string.isRequired,
     }).isRequired,
     size: PropTypes.string.isRequired,
   }).isRequired,
