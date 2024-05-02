afe23c06a2a8b4bb5fa50084a855bfd753712580
Mariana Mena <maranashokry@ahoo.com>
2024-05-01 20:59:50 -0500

    fix conflect

--- api/categoryData.js
+++ api/categoryData.js
@@ -1,26 +1,46 @@
-import { clientCredentials } from '../utils/client';
+// displayItems
+// const dbUrl = 'https://localhost:7191';
+
+// const getAllCategories = () =>
+//   new Promise((resolve, reject) => {
+// fetch(`${dbUrl}/category`, {
+//   method: 'GET',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     if (data) {
+//       resolve(Object.values(data));
+//     } else {
+//       resolve([]);
+//     }
+//   })
+//   .catch(reject);
+// });
+
+// export default getAllCategories;
+// =======
 
 const dbUrl = 'https://localhost:7191';
 
-const endpoint = clientCredentials.databaseURL;
-
-const getCategories = () =>
-  new Promise((resolve, reject) => {
-    fetch(`${endpoint}/categories`, {
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json',
-      },
+const getCategories = () => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/categories`, {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
     })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data) {
-          resolve(Object.values(data));
-        } else {
-          resolve([]);
-        }
-      })
-      .catch(reject);
-  });
+    .catch(reject);
+});
 
 export default getCategories;
---
+++ components/ItemsList .js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ItemsList() {
+  return <div>ItemsList </div>;
+}
+
+export default ItemsList;
