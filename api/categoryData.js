55f4519a44eec562b4a811d0b949d7c022542d64
Madisen Cook <77301013+maddihalee@users.noreply.github.com>
2024-05-25 15:41:53 -0500

    fixed search bar

--- api/categoryData.js
+++ api/categoryData.js
@@ -1,22 +1,21 @@
 const dbUrl = 'https://localhost:7191';
 
-const getCategories = () =>
-  new Promise((resolve, reject) => {
-    fetch(`${dbUrl}/categories`, {
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json',
-      },
+const getCategories = () => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/categories`, {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
     })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data) {
-          resolve(Object.values(data));
-        } else {
-          resolve([]);
-        }
-      })
-      .catch(reject);
-  });
+    .catch(reject);
+});
 
 export default getCategories;
