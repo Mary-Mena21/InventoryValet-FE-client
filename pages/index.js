da7f4b61c28e1a6be40ef30bcde06546a4284d6a
Madisen Cook <77301013+maddihalee@users.noreply.github.com>
2024-06-06 19:01:20 -0500

    fixing pagination 2.0

--- pages/index.js
+++ pages/index.js
@@ -52,83 +52,51 @@ function Home() {
 
   return (
     <>
-      {authUser?.firebaseId === user?.uid ? (
+    {authUser?.firebaseId === user?.uid ? (
+      <>
         <>
-          <>
-            <br />
-            <SearchBar items={items} query={query} setQuery={setQuery} />
-          </>
-          <div>
-            {pagedResult && (
+          <br />
+          <SearchBar items={items} query={query} setQuery={setQuery} />
+        </>
+        <div>
+          {pagedResult && (
+            <div>
               <div>
-                <div>
-                  {pagedResult.results?.map((item) => (
-                    <>
-                      <ItemCard itemObj={item} />
-                    </>
-                  ))}
-                </div>
-
-                {/* Pagination controls */}
-                <div>
-                  <button type="button" onClick={() => handlePageChange(page - 1)} disabled={page <= 1}>
-                    Previous
-                  </button>
-                  <span>
-                    Page {page} of {pagedResult.pageCount}
-                  </span>
-                  <button type="button" onClick={() => handlePageChange(page + 1)} disabled={page >= pagedResult.pageCount}>
-                    Next
-                  </button>
-                </div>
-                <div>
-                  <label htmlFor="pageSize">Page Size: </label>
-                  <select id="pageSize" value={pageSize} onChange={handlePageSizeChange}>
-                    <option value={5}>5</option>
-                    <option value={10}>10</option>
-                    <option value={20}>20</option>
-                  </select>
-                </div>
+                {pagedResult.results?.map((item) => (
+                  <>
+                    <ItemCard itemObj={item} />
+                  </>
+                ))}
               </div>
 
               {/* Pagination controls */}
               <div>
-                <button
-                  type="button"
-                  onClick={() => handlePageChange(page - 1)}
-                  disabled={page <= 1}
-                >
+                <button type="button" onClick={() => handlePageChange(page - 1)} disabled={page <= 1}>
                   Previous
                 </button>
-                <span>Page {page} of {pagedResult.pageCount}</span>
-                <button
-                  type="button"
-                  onClick={() => handlePageChange(page + 1)}
-                  disabled={page >= pagedResult.pageCount}
-                >
+                <span>
+                  Page {page} of {pagedResult.pageCount}
+                </span>
+                <button type="button" onClick={() => handlePageChange(page + 1)} disabled={page >= pagedResult.pageCount}>
                   Next
                 </button>
               </div>
               <div>
                 <label htmlFor="pageSize">Page Size: </label>
-                <select
-                  id="pageSize"
-                  value={pageSize}
-                  onChange={handlePageSizeChange}
-                >
-                  <option value={1}>1</option>
+                <select id="pageSize" value={pageSize} onChange={handlePageSizeChange}>
                   <option value={5}>5</option>
                   <option value={10}>10</option>
+                  <option value={20}>20</option>
                 </select>
               </div>
             </div>
           )}
-             </div>
-        </>
-      ) : (
-        <RegisterForm user={user} onUpdate={onUpdate} />
-      )}
-    </>
+        </div>
+      </>
+    ) : (
+      <RegisterForm user={user} onUpdate={onUpdate} />
+    )}
+  </>
   );
 }
 
